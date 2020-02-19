b36b8db9b6527faeb59a158b47e90572434ad627
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-02-19 11:20:03 -0600

    done

--- 14-Sinatra-Associations/db/migrate/20200218162532_create_books.rb
+++ 14-Sinatra-Associations/db/migrate/20200218162532_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title
       t.integer :year
+      t.integer :author_id
     end
   end
 end
--- 14-Sinatra-Associations/db/schema.rb
+++ 14-Sinatra-Associations/db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_162532) do
+ActiveRecord::Schema.define(version: 2020_02_19_155246) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "year"
+    t.integer "author_id"
   end
 
 end
