272219ee4394381af2b3f54768e67b4a42decf96
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-02-20 13:08:38 -0600

    done

--- 15-Sinatra-all-togheter/app/controllers/drivers_controller.rb
+++ 15-Sinatra-all-togheter/app/controllers/drivers_controller.rb
@@ -7,10 +7,46 @@ class DriversController < ApplicationController
         erb :index
     end
 
+    #form to create new driver
+    get '/drivers/new' do
+        @rides = Ride.all
+        @passengers = Passenger.all
+        erb :new
+    end
+
     #display single driver
     get '/drivers/:id' do
         @driver = current_driver
         erb :show
+    end
+
+    #create a driver
+    post '/drivers' do
+        # binding.pry
+        driver = Driver.create(params[:driver])
+
+        if params[:rides] && params[:rides].length > 0 
+            # binding.pry
+            params[:rides].each do |id|
+                r = Ride.find(id)
+                r.update(driver_id: driver.id)
+            end
+        end
+
+        if params[:passengers] && params[:passengers].length > 0
+            params[:miles].delete("")
+
+            i = 0
+
+            while i < params[:passengers].length do
+                Ride.create(driver_id: driver.id, passenger_id: params[:passengers][i], miles: params[:miles][i])
+
+                i += 1
+            end
+        end
+
+
+        redirect "/drivers/#{driver.id}"
     end
 
     #find a driver
