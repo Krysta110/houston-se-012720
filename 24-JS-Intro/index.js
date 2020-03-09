d0ccef3fe2e2e770d574c11f19e6716452a349f5
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-03-09 10:32:24 -0500

    middle code

---
+++ 24-JS-Intro/index.js
@@ -0,0 +1,12 @@
+console.log("hello world!!")
+
+let h4 = document.querySelector("h4")
+
+h4.innerText = "Blake"
+
+let pTag = document.createElement("p")
+pTag.innerText = "Something...."
+
+let div = document.querySelector("div")
+div.append(pTag)
+
