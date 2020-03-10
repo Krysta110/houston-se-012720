b0ee5af71cf212ae4c8c04a06f617b840999d60f
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-03-10 11:24:34 -0500

    done

---
+++ 25-JS-Events/src/index.js
@@ -0,0 +1,56 @@
+// document.addEventListener("DOMContentLoaded", function(){
+
+    let btn = document.querySelector("button#button1")
+
+    btn.addEventListener("click", function(){
+        console.log("Tuesday!!")
+    })
+    
+    let form = document.querySelector("form")
+    
+    form.addEventListener("submit", function(e){
+        // debugger
+        // event.preventDefault()
+        e.preventDefault()
+        // console.log("I am inside form event listener")
+        // debugger
+    
+        let div = document.getElementById("comments-container")
+    
+        let comment = document.getElementById("new-comment").value
+        // let comment = e.target[0].value
+        let li = document.createElement("li")
+        li.innerText = comment
+    
+        div.append(li)
+        e.target.reset() //to reset the form
+    })
+
+    let btnDiv = document.querySelector("div#helicopter-parent")
+
+    btnDiv.addEventListener("click", () => {
+        // debugger
+        // if (event.target.dataset.name === "alert"){
+        //     alert("Are you sure??")
+        // }
+
+        switch(event.target.dataset.name){
+            case "alert":
+                alert("Are you sure??")
+                break
+            default: 
+                console.log("clicked somewhere else!!!!")
+        }
+    })
+// })
+
+// function abc() {
+
+// }
+
+// let abc = () => {
+
+// }
+
+
+
