633246001335d688fa274ac988528aa301022ea5
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-03-30 11:06:49 -0500

    done

--- 35-Intro-react/index.js
+++ 35-Intro-react/index.js
@@ -4,22 +4,39 @@ const Text = () => {
   return React.createElement("h1", {}, "012720")
 }
 
-const NavBar = () => {
-//   <div class="ui inverted red menu">
-//   <a class='item'>
-//     <h2 class="ui header">
-//       <i class="bug icon"></i>
-//       <div class="content">
-//         bugger
-//       </div>
-//     </h2>
-//   </a>
-// </div> 
+const NavBar = (props) => {
+ return( <div className={`ui inverted ${props.color} menu`}>
+  <a className='item'>
+    <h2 className="ui header">
+      <i className={`${props.icon} icon`}></i>
+      <div className="content">
+        bugger
+      </div>
+    </h2>
+  </a>
+</div> )
 
+//"paw icon"
+// return React.createElement("div", {className:`ui inverted red menu` }, 
+//     React.createElement("a", {className: "item"}, 
+//     React.createElement("h2",{className: "ui header"}, 
+//     [
+//         React.createElement("i",{className: `bug  icon`}, null),
+//         React.createElement("div", {className:"content" }, "bugger")
 
+//     ])))
+}
+
+const App = () => {
+ return( 
+   <div>
+      <NavBar color="green" icon="paw" />
+      <Text/>
+   </div>
+ )
 }
 
 ReactDOM.render(
-  <Text/>,
+  <App/>,
   document.getElementById("container") 
 )
