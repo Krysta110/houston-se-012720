f6a34c15d8bb9205efb6877aaea37004a7d6d8de
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-03-30 10:21:06 -0500

    middle lecture

---
+++ 35-Intro-react/index.js
@@ -0,0 +1,25 @@
+console.log("Are we here?")
+
+const Text = () => {
+  return React.createElement("h1", {}, "012720")
+}
+
+const NavBar = () => {
+//   <div class="ui inverted red menu">
+//   <a class='item'>
+//     <h2 class="ui header">
+//       <i class="bug icon"></i>
+//       <div class="content">
+//         bugger
+//       </div>
+//     </h2>
+//   </a>
+// </div> 
+
+
+}
+
+ReactDOM.render(
+  <Text/>,
+  document.getElementById("container") 
+)
